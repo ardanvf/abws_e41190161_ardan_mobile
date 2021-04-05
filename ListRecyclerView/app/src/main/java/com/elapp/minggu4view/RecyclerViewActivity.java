ac56886378d10b47aa437594c2f8300f92c7571b
ardanvf <71808926+ardanvf@users.noreply.github.com>
2021-04-05 14:06:13 +0700

    List Recycle

---
+++ ListRecyclerView/app/src/main/java/com/elapp/minggu4view/RecyclerViewActivity.java
@@ -0,0 +1,38 @@
+package com.elapp.minggu4view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+
+    private MahasiswaAdapter mAdapter;
+    private RecyclerView rvMahasiswa;
+    private ArrayList<Mahasiswa> mahasiswaArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+
+        addData();
+
+        rvMahasiswa = findViewById(R.id.rv_mahasiswa);
+
+        mAdapter = new MahasiswaAdapter(mahasiswaArrayList);
+        rvMahasiswa.setLayoutManager(new LinearLayoutManager(this));
+        rvMahasiswa.setAdapter(mAdapter);
+    }
+
+    private void addData() {
+        mahasiswaArrayList = new ArrayList<>();
+        mahasiswaArrayList.add(new Mahasiswa("Ardan", "E41190161", "089987987987"));
+        mahasiswaArrayList.add(new Mahasiswa("Venora", "E41190162", "081234234234"));
+        mahasiswaArrayList.add(new Mahasiswa("Falahudin", "E41190163", "0876544566544"));
+
+    }
+}
